cc022f1b73389b06db5e0d0777993f0febfdb1fc
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-07-08 16:42:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", encoding="utf-8") as r:
     requires = [i.strip() for i in r]
 
 # find repository version
-with open("pyshot/__init__.py", encoding="utf-8") as f:
+with open("shortlib/__init__.py", encoding="utf-8") as f:
     version = re.findall(r"__version__ = \"(.+)\"", f.read())[0]
 
 # read readme.md
@@ -17,16 +17,16 @@ with open("README.md", encoding="utf-8") as f:
     readme = f.read()
   
 setuptools.setup(
-    name="PyShots",
+    name="Shortlib",
     version=version,
     author="Pr0fess0r-99",
     long_description=readme,
     license='GNU General Public License v3.0',
     description='python short cut',                           
     package_data={
-      "pyshot": ["py.typed"],
+      "shortlib": ["py.typed"],
     },
-    url="https://github.com/PR0FESS0R-99/PyShot",
+    url="https://github.com/PR0FESS0R-99/ShortLib",
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -46,5 +46,5 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=requires,
-    py_modules=["pyshot"],
+    py_modules=["shortlib"],
 )
